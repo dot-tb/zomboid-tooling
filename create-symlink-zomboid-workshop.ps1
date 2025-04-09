b25ccc12ea007c2577a13ece941d17f5ef80dd6d
Tibère Borel <tibere.borel@outlook.com>
2025-04-09 14:40:00 +0200

    Creating a B41 symlink folder

--- create-symlink-zomboid-workshop.ps1
+++ create-symlink-zomboid-workshop.ps1
@@ -7,10 +7,12 @@ if (!(Test-Path ".\workshop.txt")) {
 }
 
 $FolderName = (Get-Item $FolderPath).Name
+$FolderNameSpaces = $FolderName -Replace '-', ' '
+$FolderNameTitleCase = (Get-Culture).TextInfo.ToTitleCase($FolderNameSpaces) -Replace ' '
 
 New-Item -Path "$env:USERPROFILE\Zomboid\Workshop\$FolderName" -ItemType SymbolicLink -Value $FolderPath
 if (Test-Path "$env:USERPROFILE\ZomboidB41") {
   Write-Host "Found B41 cache folder, writting symlink there too"
-  New-Item -Path "$env:USERPROFILE\ZomboidB41\Workshop\$FolderName" -ItemType SymbolicLink -Value $FolderPath
+  New-Item -Path "$env:USERPROFILE\ZomboidB41\mods\$FolderName" -ItemType SymbolicLink -Value "$FolderPath\Contents\mods\Delran$FolderNameTitleCase"
 }
 
