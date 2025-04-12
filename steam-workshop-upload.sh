c88f07be8a02a83fbab0b913ef9c0618bca6e9e0
Tibère Borel <tibere.borel@outlook.com>
2025-04-12 18:32:14 +0200

    new script to upload workshop item

---
+++ steam-workshop-upload.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/zsh
+
+[[ ! -f "workshop.txt" ]] && echo "Please run this script from the root folder of the mod" && exit
+
+APP_ID=108600
+WORKSHOP_ID=$(cat workshop.txt | grep -E '^id=' | sed -E 's/^id=//g')
+
+cat << EOT > /tmp/template.vdf
+"workshopitem"
+{
+    "appid" "$APP_ID"
+    "publishedfileid" "$WORKSHOP_ID"
+    "contentfolder" "$(cygpath -da $PWD)"
+}
+EOT
+
+steamcmd +login $(op read op://personal/steam/username) $(op read op://personal/steam/password) +workshop_build_item "$(cygpath -da /tmp/template.vdf)" +quit
