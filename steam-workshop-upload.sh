292e138c012d7ca7bc6bbbd7702beb74f44830de
Tibère Borel <tibere.borel@outlook.com>
2025-04-29 15:15:50 +0200

    Now takes the description from the workshop file

--- steam-workshop-upload.sh
+++ steam-workshop-upload.sh
@@ -5,12 +5,16 @@
 APP_ID=108600
 WORKSHOP_ID=$(cat workshop.txt | grep -E '^id=' | sed -E 's/^id=//g')
 
+DESCRIPTION=$(cat workshop.txt | grep description= | sed -E 's/^description=//')
+
+
 cat << EOT > /tmp/template.vdf
 "workshopitem"
 {
     "appid" "$APP_ID"
     "publishedfileid" "$WORKSHOP_ID"
     "contentfolder" "$(cygpath -da $PWD)\\Contents"
+    "description" "$DESCRIPTION"
 }
 EOT
 
